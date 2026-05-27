44abb91bbbb90736e9db4ecbca2ed520a724fe7e
mithridatem <mathieumithridate@adrar-formation.com>
2026-05-27 11:23:55 +0200

    feat: update Category

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -25,14 +25,16 @@ class Category
         return $this->name;
     }
 
-    public function setId(int $id): void
+    public function setId(int $id): self
     {
         $this->id = $id;
+        return $this;
     }
 
-    public function setName(string $name): void
+    public function setName(string $name): self
     {
         $this->name = $name;
+        return $this;
     }
 
     //toString
